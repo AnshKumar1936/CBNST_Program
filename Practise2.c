5ab2b8a50f11211b29cacfd6c00ca0257c688846
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-03-21 19:22:14 +0530

    Add files via upload

---
+++ Practise2.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<math.h>
+#define EPSILON 0.0001
+
+float findValueAt(float x){
+
+    return x*x*x - 2*x - 5;
+}
+
+float RegFalsi(float x1 , float x2){
+    return (x1 * findValueAt(x2) - x2 * findValueAt(x1)) / (findValueAt(x2) - findValueAt(x1));
+
+
+}
+
+int main(){
+    int MaxItr,i=1;
+    float x1,x2,x3,x;
+
+    printf("Enter the no of Iteration:");
+    scanf("%d", &MaxItr);
+
+
+
+do{
+    printf("Enter the value of x1 and x2: ");
+    scanf("%f%f", &x1,&x2);
+
+    if(findValueAt(x1) * findValueAt(x2)>0){
+        printf("Roots are Invalid\n");
+        continue;
+    }
+    else{
+        printf("Roots Lie between %f and %f \n" , x1,x2);
+        break;
+    }
+}while(1);
+
+x=RegFalsi(x1,x2);
+
+do
+{
+        if(findValueAt(x) * findValueAt(x1)<0)
+            x2=x;
+        else
+            x1=x;
+
+        printf("Iteration=%d Roots=%f\n", i,x);
+        x3 = RegFalsi(x1,x2);
+        if(fabs(x3-x)<EPSILON)
+        {
+            printf("Root=%f Total Iteration=%d",x,i );
+            return 0;
+        }
+        x=x3;
+        i++;
+
+    }while(i<=MaxItr);
+    printf("Root=%f  Total Iterations=%d" ,x,--i);
+
+    return 0;
+}
